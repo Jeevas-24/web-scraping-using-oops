bf32ab24107320e0816e2f0a258124e402fdd657
jeeva <jeevasathappan24@gmail.com>
2024-11-08 11:55:03 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,81 @@
+import time
+import requests
+import selectorlib
+import smtplib, ssl
+import os
+import sqlite3
+
+url = 'http://programmer100.pythonanywhere.com/tours/'
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+connection = sqlite3.connect('data.db')
+
+
+# Headers are used for specific web server that don't like script programs, so these helps here showing the program as browser
+
+class Event:
+    def scrape(self, url):
+        """Scrape the page source from the url"""
+        response = requests.get(url, headers=HEADERS)
+        source = response.text
+        return source
+
+    def extract(self, source):
+        extractor = selectorlib.Extractor.from_yaml_file('extract.yaml')
+        value = extractor.extract(source)['tours']
+        return value
+
+
+class Email:
+    def send(self, raw_message):
+        host = 'smtp.gmail.com'
+        port = 465
+        username = 'jeevasathappan2000@gmail.com'
+        password = 'ypxn naqt nwau ghhx'
+        receiver = 'jeevasathappan2000@gmail.com'
+        context = ssl.create_default_context()
+        message = f"""\
+    Subject: New Event on board
+    
+    {raw_message}
+    """
+        with smtplib.SMTP_SSL(host, port, context=context) as server:
+            server.login(username, password)
+            server.sendmail(username, receiver, message)
+        print('Mail sent')
+
+
+def read(extracted):
+    row = extracted.split(',')
+    row = [r.strip() for r in row]
+    band, city, date = row
+    cursor = connection.cursor()
+    cursor.execute('select * from events where band=? and city=? and date=?',
+                   (band, city, date))
+    rows = cursor.fetchall()
+    print(rows)
+    return rows
+
+
+def store(extracted):
+    row = extracted.split(',')
+    row = [r.strip() for r in row]
+    cursor = connection.cursor()
+    cursor.execute('insert into events values (?,?,?)', row)
+    connection.commit()
+
+
+if __name__ == '__main__':
+    while True:
+        event = Event()
+        scraped = event.scrape(url)
+        extracted = event.extract(scraped)
+        print(extracted)
+
+        if extracted != 'No upcoming tours':
+            rows = read(extracted)
+            if not rows:
+                store(extracted)
+                email = Email()
+                email.send(extracted)
+        time.sleep(2)
